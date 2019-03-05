25a7f3cc3c45577075166fd1ac24dabc2311bbd6
Jon Allured <jon.allured@gmail.com>
2019-03-05 16:57:12 -0600

    WIP: pass in wrapper
    What if we could just pass in the wrapper that we wanted for the
    spinner?
    
    /cc @pepopowitz

--- src/Artsy/Relay/renderWithLoadProgress.tsx
+++ src/Artsy/Relay/renderWithLoadProgress.tsx
@@ -17,56 +17,44 @@ const SpinnerContainer = styled.figure`
 
 export const LoadingClassName = "relay-loading"
 
+const handleError = error => {
+  // In tests we want errors to clearly bubble up.
+  if (typeof jest !== "undefined") {
+    throw error
+  }
+
+  const networkError = error as any
+  if (error.message) {
+    console.error(error.message)
+  }
+  if (networkError.response && networkError.response._bodyInit) {
+    let data = networkError.response._bodyInit
+    if (data) {
+      try {
+        data = JSON.parse(data)
+        console.error(`Metaphysics Error data:`, data)
+        // tslint:disable-next-line:no-empty
+      } catch (e) {}
+    }
+  }
+}
+
 export function renderWithLoadProgress<P>(
   Container: RelayContainer<P>,
+  Wrapper: any = SpinnerContainer,
   initialProps: object = {}
 ): (readyState: ReadyState<P>) => React.ReactElement<RelayContainer<P>> | null {
-  // TODO: We need design for retry-ing, or the go-ahead to re-use the design of
-  //       the iOS app.
-  //
-  // let retrying = false
   return ({ error, props, retry }) => {
     if (error) {
-      // In tests we want errors to clearly bubble up.
-      if (typeof jest !== "undefined") {
-        throw error
-      }
-
-      const networkError = error as any
-      if (error.message) {
-        console.error(error.message)
-      }
-      if (networkError.response && networkError.response._bodyInit) {
-        let data = networkError.response._bodyInit
-        if (data) {
-          try {
-            data = JSON.parse(data)
-            console.error(`Metaphysics Error data:`, data)
-            // tslint:disable-next-line:no-empty
-          } catch (e) {}
-        }
-      }
-
-      // if (retrying) {
-      //   retrying = false
-      //   // TODO: Even though this code path is reached, the retry button keeps spinning. iirc it _should_ disappear when
-      //   //      `onRetry` on the instance is unset.
-      //   //
-      //   // This will re-use the native view first created in the renderFailure callback, which means it can
-      //   // continue its ‘retry’ animation.
-      //   return <LoadFailureView style={{ flex: 1 }} />
-      // } else {
-      //   retrying = true
-      //   return <LoadFailureView onRetry={retry} style={{ flex: 1 }} />
-      // }
+      handleError(error)
       return null
     } else if (props) {
       return <Container {...initialProps} {...props as any} />
     } else {
       return (
-        <SpinnerContainer className={LoadingClassName}>
+        <Wrapper className={LoadingClassName}>
           <Spinner />
-        </SpinnerContainer>
+        </Wrapper>
       )
     }
   }
